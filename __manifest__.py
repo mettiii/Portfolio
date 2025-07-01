5d843abcd76367ca52b3a5c546c9bf13a5c3a0d8
mettttttttti <metikebede83@gmail.com>
2025-07-01 03:39:59 +0300

    updated file

--- __manifest__.py
+++ __manifest__.py
@@ -5,10 +5,15 @@
     'license': 'LGPL-3',
     'version': '1.0',
     'depends': [
-
+        'base',
+        'mail',
+        'hr'
     ],
     'data': [
-
+        'security/ir.model.access.csv',
+        'views/employee.xml',
+        'views/exam_list.xml',
+        'views/menu_views.xml',
     ],
     'installable': True,
     'application': True,
